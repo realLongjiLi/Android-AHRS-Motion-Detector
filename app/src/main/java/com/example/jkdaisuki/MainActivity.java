3b09132ba353087c34d79f09cede6dfbcc43d415
Longji Li <longjil2@illinois.edu>
2019-07-20 02:16:13 +0800

    输出CSV

--- app/src/main/java/com/example/jkdaisuki/MainActivity.java
+++ app/src/main/java/com/example/jkdaisuki/MainActivity.java
@@ -5,13 +5,19 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.lang.reflect.Array;
+import java.text.DecimalFormat;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     // acc sensor manager
@@ -28,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
     // gyroscope
     Sensor gySensor;
 
-    double DATA[][] = new double[3][3];
+    DecimalFormat decimalFormat = new DecimalFormat("#.000000");
+
+    LinkedList<double[]> linkedList = new LinkedList<>();
+
+    double[][] DATA = new double[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         acSensor = acSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         grSensor = grSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         gySensor = gySensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+
     }
 
     // 校准
@@ -60,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
             ((TextView) findViewById(R.id.txView)).setText("[" + event.values[0] + ", "
                     + event.values[1] + ", " + event.values[2] + "]");
             for (int i = 0; i < 3; i++) {
-                DATA[0][i] = event.values[i];
+                DATA[1][i] = event.values[i];
             }
         }
 
@@ -78,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
             ((TextView) findViewById(R.id.txView2)).setText("[" + event.values[0] + ", "
                     + event.values[1] + ", " + event.values[2] + "]");
             for (int i = 0; i < 3; i++) {
-                DATA[1][i] = event.values[i];
+                DATA[2][i] = event.values[i];
             }
         }
 
@@ -96,14 +108,20 @@ public class MainActivity extends AppCompatActivity {
             ((TextView) findViewById(R.id.txView3)).setText("[" + event.values[0] + ", "
                     + event.values[1] + ", " + event.values[2] + "]");
             for (int i = 0; i < 3; i++) {
-                DATA[2][i] = event.values[i];
+                DATA[0][i] = event.values[i];
             }
+            double[] tmp = AHRS.MadgwickAHRSupdate(DATA[0][0], DATA[0][1], DATA[0][2], DATA[1][0],
+                    DATA[1][1], DATA[1][2], DATA[2][0], DATA[2][1], DATA[2][2]);
+            String output = decimalFormat.format(tmp[0]) + ", " + decimalFormat.format(tmp[1])
+                    + ", " + decimalFormat.format(tmp[2]) + ", "
+                    + decimalFormat.format(tmp[3]);
+            ((TextView) findViewById(R.id.textViewComputed)).setText(output);
+            linkedList.add(tmp);
         }
 
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
             // TODO Auto-generated method stub
-
         }
     };
 
@@ -111,14 +129,34 @@ public class MainActivity extends AppCompatActivity {
         acSensorManager.unregisterListener(acSensorEventListener, acSensor);
         grSensorManager.unregisterListener(grSensorEventListener, grSensor);
         gySensorManager.unregisterListener(gySensorEventListener, gySensor);
-
+        /*
         double[] tmp = AHRS.MadgwickAHRSupdate(DATA[0][0], DATA[0][1], DATA[0][2], DATA[1][0],
                 DATA[1][1], DATA[1][2], DATA[2][0], DATA[2][1], DATA[2][2]);
-        ((TextView) findViewById(R.id.textViewComputed)).setText(tmp[0] + ", " + tmp[1] + ", " + tmp[2] + ", " + tmp[3]);
+        ((TextView) findViewById(R.id.textViewComputed)).setText(tmp[0] + ", " + tmp[1] + ", " + tmp[2] + ", " + tmp[3]); */
     }
 
     public void onClickReset(View v) {
         AHRS.resetq();
         Toast.makeText(this, "reset", Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickSave(View v) {
+        // save linkedList
+        StringBuilder stringBuilder = new StringBuilder();
+        for (double[] each : linkedList) {
+            String str = each[1] + ", " + each[2] + ", " + each[3] + "\n";
+            stringBuilder.append(str);
+        }
+
+        try {
+            Log.d("DEBUT", stringBuilder.toString());
+            File file = new File(Environment.getExternalStorageDirectory(), "data.csv");
+            FileOutputStream fos = new FileOutputStream(file);
+            byte[] bytes = stringBuilder.toString().getBytes();
+            fos.write(bytes);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
